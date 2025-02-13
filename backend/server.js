91d9afee521ac32d3a4baab660453589f617b777
vipinrao009 <vipinrao2018@gmail.com>
2025-02-13 13:48:19 +0530

    Establish MongoDB connection and update server initialization

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 import app from "./app.js";
 import cloudinary from "cloudinary"
+import connectToDB from "./config/db.connection.js";
 
 const PORT = process.env.PORT
+
+connectToDB()
+  .then(() => {
+    app.listen(PORT || 7000, () => {
+      console.log(`Server is runnig at http://localhost:${PORT}`);
+    });
+  })
+
+  .catch((e) => {
+    console.log("MongoDB connection failed!!!");
+  });
 
 cloudinary.v2.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_SECRET_KEY
-})
-
-app.listen(()=>{
-    console.log(`Server is running on ${PORT}`)
 })
